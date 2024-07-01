6168c8a1cd8af1d61b3fc2c124a1cd5ea2d8218c
Michael Florya <mih2303@mail.ru>
2024-07-01 22:17:22 +0300

    create page blog detail

--- it/main/urls.py
+++ it/main/urls.py
@@ -6,5 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.index, name='index'),
+
     path('blog/', views.blog, name='blog'),
+    path('blog/<slug:slug>/', views.blog_detail, name='blog_detail'),
 ]
--- it/main/views.py
+++ it/main/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 
 
@@ -20,3 +20,14 @@ def blog(request):
     }
 
     return render(request, 'main/blog.html', context)
+
+
+def blog_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug)
+
+    context = {
+        'title': post.title,
+        'post': post,
+    }
+
+    return render(request, 'main/blog_detail.html', context)
